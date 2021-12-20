518ff7403e80e9d3f985d91c680748dd3e88d2a3
Dmitrii <dimterion59gmail.com>
2021-12-20 16:31:14 +0100

    Added validation for all fields

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,10 +12,12 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
+const errorCity = document.querySelector("#error_city");
+const errorTerms = document.querySelector("#error_terms");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-closeBtn.addEventListener('click', closeMenu);
+closeBtn.addEventListener("click", closeMenu);
 
 // launch modal form
 function launchModal() {
@@ -25,4 +27,17 @@ function launchModal() {
 // close modal form
 function closeMenu() {
   modalbg.style.display = "none";
+}
+
+// validation
+function validate() {
+  var city = document.forms["reserve"]["location"];
+  var terms = document.forms["reserve"]["terms-check"];
+  if ((city[0].checked == false) && (city[1].checked == false) && (city[2].checked == false) && (city[3].checked == false) && (city[4].checked == false) && (city[5].checked == false)) {
+    errorCity.innerHTML = "You must choose one option.";
+    return false;
+  } else if (terms.checked == false) {
+    errorTerms.innerHTML = "You must check to agree to terms and conditions.";
+    return false;
+  }
 }
